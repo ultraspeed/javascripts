5d1771736fe29d8e5c0b82d78d741dbd2359b0d8
dinesh <dinesh@8a7d78bc-e03d-0410-8679-c8f2d7fc2081>
2008-04-23 16:57:21 +0000

    * Updated scripts so that the back button isn't displayed on page 6 of the ordering forms
    git-svn-id: svn://dev.ultraspeed.com/lib/frontend/trunk/js@245 8a7d78bc-e03d-0410-8679-c8f2d7fc2081

--- ordering.js
+++ ordering.js
@@ -4,47 +4,47 @@
 
 
 $(function() {
-  
-  
+
+
   // Table striping (we don't want to stripe tfoot)
   $("table tr:odd").not(":first").addClass("odd");
-  
-  
+
+
   // Hover over table rows (we don't want to hover over the thead)
   $("tr").not(":first").mouseover(function() { $(this).addClass("over"); }).mouseout(function() { $(this).removeClass("over"); });
-  
-  
+
+
   // hides all elements that are marked to be hidden
   $(".hidden").hide();
-  
-  
+
+
   // highlights the containers where the input is checked
   $("input:checked").each(function() {
         $(this).parent().addClass("selected");
         // if there are any checked elements, un-hide it's children
         toggle_hiddens(this, "show");
       });
-  
-  
+
+
   // highlight containers & unhide/hide elements marked hidden
   $(":radio, :checkbox").click(function() {
-        if ( $(this).attr("checked") == true && $("form").attr("id") != "step5" ) { 
+        if ( $(this).attr("checked") == true && $("form").attr("id") != "step5" ) {
           // removes the highlight class from all radio buttons
           var radios = $(this).parents("dl").find(":radio");
           if ( radios ) radios.parent().removeClass("selected");
-          
+
           toggle_hiddens(this, "show");
-          
+
           $(this).parent().addClass("selected");
         }
         else if ( $("form").attr("id") != "step5" ) {
           toggle_hiddens(this, "hide");
-          
+
           $(this).parent().removeClass("selected");
         };
       });
-  
-  
+
+
   // toggles selected class on input text fields, select.input and textareas (other inputs are handled by the above)
   $("input.text, select.input, textarea.input").focus(function() {
     $(this).parent().addClass("selected");
@@ -52,10 +52,10 @@ $(function() {
   $("input.text, select.input, textarea.input").blur(function() {
     $(this).parent().removeClass("selected");
   });
-  
-  
+
+
   // creates the back button inside forms (won't be generated if JS is not enabled)
-  if ( $("form").attr("id") != "step1" )
+  if ( $("form").attr("id") != "step1" && $("form").attr("id") != "step6" )
   {
     $("#go_previous").html('<button type="button">Previous</button>');
     $("#go_previous button").click(function() {
@@ -67,8 +67,8 @@ $(function() {
            };
     });
   }
-  
-  
+
+
   // IE fix for :hover on buttons
   if ( $.browser.msie ) {
     $("button").hover(
@@ -76,8 +76,8 @@ $(function() {
       function() { $(this).css('cursor',"auto") }
     );
   }
-  
-  
+
+
   // asks for confirmation before resetting the form
   $('#reset_form a').click(function() {
     window.location = $(this).attr('href');
@@ -95,7 +95,7 @@ $(function() {
 function toggle_hiddens(element, action)
 {
   hiddens = $(element).parent().find(".hidden");
-  
+
   if ( hiddens ) {
         if (action == "show")
         {
